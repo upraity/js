e6461285019d01f6debcae2ac52067cecddf9dac
Dj <134566303+upraity@users.noreply.github.com>
2024-05-16 22:32:23 +0530

    For Loop

---
+++ loop.js
@@ -0,0 +1,11 @@
+/* There are four types of loop in javascript
+    1. For Loop
+    2. forEach Loop
+    3. While Loop
+    4. do while Loop
+*/
+
+// for loop
+for(let i = 0; i <= 5; i++){
+  console.log(i + " SabkaCode");
+}
